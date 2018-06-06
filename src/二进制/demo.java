0510b1bb32563fdd58c3f6293d5df13354b4e86e
1309465818@qq.com <ZYDX@dx>
2018-06-06 20:43:45 +0800

    nothing to

---
+++ src/二进制/demo.java
@@ -0,0 +1,18 @@
+package 二进制;
+
+public class demo {
+	public static void main(String[] args) {
+		/*
+		 * 数据在计算机中时二进制的
+		 */
+		int n= 50;
+		System.out.println(Integer.toBinaryString(n));
+		/*
+		 * 输出0`50二进制
+		 */
+		for(int i=0;i<50;i++) {
+			String bint = Integer.toBinaryString(i);
+		}
+		
+	}
+}
